3e6bfd62adb1c530fdaa54041b0f76a3cdacea4b
Ilan <ilan.pillemer@gmail.com>
2023-01-05 20:53:32 +0000

    solution that will own work with sample for part2

--- d22/main.go
+++ d22/main.go
@@ -16,6 +16,7 @@ type tile struct {
 	down     *tile
 	left     *tile
 	right    *tile
+	side     int
 }
 
 const (
@@ -26,12 +27,306 @@ const (
 )
 
 func main() {
+	//	part1()
+	part2()
+}
+
+func part2() {
 	maxx := 0
 	maxy := 0
 	facing := 0 // facing to the right
 	tiles := map[image.Point]*tile{}
 	fmt.Println("d22")
-	b, _ := os.ReadFile("INPUT")
+	b, _ := os.ReadFile("SAMPLE")
+	xs := strings.Split(string(b), "\n\n")
+	board, path := xs[0], xs[1]
+	grid := strings.Split(board, "\n")
+	fmt.Println(board)
+	fmt.Println(path)
+	path = path + "$"
+	//parse to get all tiles
+
+	for i, t := range grid {
+		for j, v := range t {
+			if v != ' ' {
+				maxx = max(maxx, j)
+				maxy = max(maxy, i)
+				e := &tile{
+					position: image.Pt(j, i),
+					value:    v,
+				}
+				tiles[image.Pt(j, i)] = e
+			}
+		}
+	}
+
+	// add wrapping links
+	//top
+	// this will be where y is 0
+	fmt.Println("SIDE1")
+	//sample rules rules
+	for _, v := range tiles {
+		if v.position.Y >= 0 && v.position.Y < 4 {
+			v.side = 1
+			tiles[v.position] = v
+			continue
+		}
+		if v.position.Y >= 8 && v.position.Y < 12 {
+			v.side = 5
+			if v.position.X >= 12 {
+				v.side = 6
+			}
+			tiles[v.position] = v
+			continue
+		}
+		switch {
+		case v.position.X >= 4 && v.position.X < 8:
+			v.side = 3
+		case v.position.X >= 8:
+			v.side = 4
+		default:
+			v.side = 2
+		}
+		tiles[v.position] = v
+		continue
+	}
+
+	//parse to link tiles
+	fmt.Println(maxx, maxy)
+	for k, v := range tiles {
+		// l + r
+		left := v.position.X - 1
+		for {
+			if left < 0 {
+				break
+			}
+			if leftTile, ok := tiles[image.Pt(left, v.position.Y)]; ok &&
+				left != v.position.X &&
+				leftTile.side == v.side {
+				v.left = leftTile
+				tiles[k] = v
+				break
+			}
+			left--
+		}
+		right := v.position.X + 1
+		for {
+			if right > maxx {
+				break
+			}
+			if rightTile, ok := tiles[image.Pt(right, v.position.Y)]; ok && right != v.position.X &&
+				rightTile.side == v.side {
+				v.right = rightTile
+				tiles[k] = v
+				break
+			}
+			right++
+		}
+		// u + d
+		up := v.position.Y - 1
+		for {
+			if up < 0 {
+				break
+			}
+			if upTile, ok := tiles[image.Pt(v.position.X, up)]; ok && up != v.position.Y &&
+				upTile.side == v.side {
+				v.up = upTile
+				tiles[k] = v
+				break
+			}
+			up--
+		}
+		down := v.position.Y + 1
+		for {
+			if down > maxy {
+				break
+			}
+			if downTile, ok := tiles[image.Pt(v.position.X, down)]; ok && down != v.position.Y &&
+				downTile.side == v.side {
+				v.down = downTile
+				tiles[k] = v
+				break
+			}
+			down++
+		}
+	}
+
+	//	for _, v := range tiles {
+	//		fmt.Println(v.side, v.position)
+	//
+	//	}
+
+	//os.Exit(0)
+	//	for _, v := range tiles {
+	//		fmt.Printf("up: %v -> %v\n", v.position, v.up)
+	//		fmt.Printf("down: %v -> %v\n", v.position, v.down)
+	//		fmt.Printf("left: %v -> %v\n", v.position, v.left)
+	//		fmt.Printf("right: %v -> %v\n", v.position, v.right)
+	//
+	//	}
+
+	leftmost := 2147483647
+	for k := range tiles {
+		if k.Y == 0 {
+			leftmost = min(leftmost, k.X)
+		}
+	}
+	current := tiles[image.Pt(leftmost, 0)]
+	var move func(int)
+	move = func(count int) {
+		if count == 0 {
+			return
+		}
+		fmt.Printf("facing %d, position side %d %v\n", facing, current.position, current.side)
+		newfacing := facing
+		switch facing {
+		case 0:
+			if current.right == nil {
+				switch current.side {
+				case 4:
+					current.right = tiles[image.Pt(current.position.X+3, current.position.Y+3)]
+					newfacing = DOWN
+				case 2:
+					current.right = tiles[image.Pt(current.position.X+1, current.position.Y)]
+					newfacing = RIGHT
+				default:
+					panic("oops")
+				}
+			}
+			if current.right.value != '#' {
+				if current.side != current.down.side {
+					fmt.Printf("side [%d] -> side [%d]\n", current.side, current.down.side)
+
+				}
+				facing = newfacing
+				current = current.right
+			}
+
+			move(count - 1)
+		case 1:
+			if current.down == nil {
+				switch current.side {
+				case 1:
+					current.down = tiles[image.Pt(current.position.X, current.position.Y+1)]
+					newfacing = DOWN
+				case 5:
+					current.down = tiles[image.Pt(current.position.X-9, current.position.Y-4)]
+					newfacing = UP
+				default:
+					panic("oops")
+				}
+			}
+			if current.down.value != '#' {
+				if current.side != current.down.side {
+					fmt.Printf("side [%d] -> side [%d]\n", current.side, current.down.side)
+				}
+				facing = newfacing
+				current = current.down
+			}
+
+			move(count - 1)
+		case 2:
+			if current.left == nil {
+				switch current.side {
+				case 6:
+					current.left = tiles[image.Pt(current.position.X-1, current.position.Y)]
+					newfacing = LEFT
+				default:
+					panic("oops")
+				}
+			}
+			if current.left.value != '#' {
+
+				current = current.left
+			}
+
+			move(count - 1)
+		case 3:
+			if current.up == nil {
+				switch current.side {
+				case 3:
+					fmt.Println(image.Pt(current.position.X+8, current.position.Y+4))
+					current.up = tiles[image.Pt(current.position.X+8, current.position.Y+7)]
+					newfacing = UP
+				default:
+					panic("oops")
+				}
+			}
+			if current.up.value != '#' {
+				facing = newfacing
+				current = current.up
+			}
+			facing = newfacing
+
+			move(count - 1)
+		default:
+			panic("oops")
+
+		}
+	}
+
+	process := func() {
+		p := 0
+		for {
+			if p >= len(path) {
+				break
+			}
+			num := ""
+			for {
+				if unicode.IsDigit(rune(path[p])) && p < len(path) {
+					num = num + string(path[p])
+					p++
+				} else {
+					println(num)
+					move(atoi(num))
+					break
+				}
+			}
+			switch path[p] {
+			case 'R':
+				switch facing {
+				case 0:
+					facing = 1
+				case 1:
+					facing = 2
+				case 2:
+					facing = 3
+				case 3:
+					facing = 0
+				}
+			case 'L':
+				switch facing {
+				case 0:
+					facing = 3
+				case 1:
+					facing = 0
+				case 2:
+					facing = 1
+				case 3:
+					facing = 2
+				}
+			case '$':
+				fmt.Println("finished")
+				fmt.Printf("final position index 1: facing %d, position %v %v\n", facing, current.position.X+1, current.position.Y+1)
+				fmt.Println("password", facing+(4*(current.position.X+1))+((current.position.Y+1)*1000))
+			default:
+				panic("oops")
+			}
+			fmt.Printf("%c\n", path[p])
+			p++
+		}
+
+	}
+	process()
+}
+
+func part1() {
+	maxx := 0
+	maxy := 0
+	facing := 0 // facing to the right
+	tiles := map[image.Point]*tile{}
+	fmt.Println("d22")
+	b, _ := os.ReadFile("SAMPLE")
 	xs := strings.Split(string(b), "\n\n")
 	board, path := xs[0], xs[1]
 	grid := strings.Split(board, "\n")
@@ -122,37 +417,34 @@ func main() {
 		}
 	}
 	current := tiles[image.Pt(leftmost, 0)]
-	move := func(count int) {
+	var move func(int)
+	move = func(count int) {
+		if count == 0 {
+			return
+		}
 		fmt.Printf("facing %d, position %v\n", facing, current.position)
 		switch facing {
 		case 0:
-			fmt.Println("move right", count)
-			for i := 0; i < count; i++ {
-				if current.right.value != '#' {
-					current = current.right
-				}
+			if current.right.value != '#' {
+				current = current.right
 			}
+			move(count - 1)
 		case 1:
-			fmt.Println("move down", count)
-			for i := 0; i < count; i++ {
-				if current.down.value != '#' {
-					current = current.down
-				}
+			if current.down.value != '#' {
+				current = current.down
 			}
+			move(count - 1)
 		case 2:
-			fmt.Println("move left", count)
-			for i := 0; i < count; i++ {
-				if current.left.value != '#' {
-					current = current.left
-				}
+
+			if current.left.value != '#' {
+				current = current.left
 			}
+			move(count - 1)
 		case 3:
-			fmt.Println("move up", count)
-			for i := 0; i < count; i++ {
-				if current.up.value != '#' {
-					current = current.up
-				}
+			if current.up.value != '#' {
+				current = current.up
 			}
+			move(count - 1)
 		default:
 			panic("oops")
 
