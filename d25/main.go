c09866292ed9f7099945a1b6625812a3b14d59b1
Ilan <ilan.pillemer@gmail.com>
2023-01-12 15:05:04 +0000

    tidy

--- d25/main.go
+++ d25/main.go
@@ -14,15 +14,11 @@ func main() {
 	lines := strings.Split(string(b), "\n")
 	var total int64
 	fmt.Println(lines)
-	//	fmt.Println("2=-01 ==> ", fromSnafu("2=-01"))
-	//	fmt.Println("1121-1110-1=0 ==> ", fromSnafu("1121-1110-1=0"))
 	fmt.Println("2==0=0===02--210---1 ==> ", fromSnafu("2==0=0===02--210---1"))
-	//os.Exit(0)
 	for _, line := range lines {
 		total = total + fromSnafu(line)
 	}
 	fmt.Println(total)
-
 	fmt.Println(toSnafu(total))
 }
 
@@ -31,7 +27,6 @@ func fromSnafu(num string) int64 {
 
 	for i, j := len(num)-1, 0.0; i >= 0; i, j = i-1, j+1 {
 		col := int64(math.Pow(5, j))
-		//	fmt.Printf("%d * %d\n", f(num[i]), col)
 		total = total + (f(num[i]) * col)
 	}
 	return total
@@ -41,9 +36,7 @@ func toSnafu(num int64) string {
 	base5 := strconv.FormatInt(num, 5)
 	fmt.Println(base5)
 	carry := 0
-	_ = carry
 	str := ""
-	_ = str
 	for i, j := len(base5)-1, 0.0; i >= 0; i, j = i-1, j+1 {
 		n := atoi(string(base5[i]))
 		n = n + carry
@@ -65,8 +58,6 @@ func toSnafu(num int64) string {
 			str = "0" + str
 			carry = 1
 		}
-		//col := int64(math.Pow(5, j))
-		//fmt.Printf("%d %d\n", n, col)
 	}
 
 	return str
